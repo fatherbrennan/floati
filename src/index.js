2f1892400335fd06d349d4955d673267ad4a6893
fatherbrennan <bjt.lepore@gmail.com>
2022-05-11 10:56:17 +0930

    add App component to handle all views

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './routes/Home/Home';
+import App from './routes/App/App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Home />
+        <App />
     </React.StrictMode>
 );
 
---
+++ src/routes/App/App.js
@@ -0,0 +1,20 @@
+/**
+ * Floati
+ * MIT Licensed
+ *
+ * @fatherbrennan
+ */
+import React from 'react';
+import Home from './../Home/Home';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <Home />;
+    }
+}
+
+export default App;
